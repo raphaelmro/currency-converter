bb4a8a040a3c946da809e2e2f61247cfb1fe5fa5
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-05-11 01:08:06 -0300

    Customize select button, get currency from global state

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <v-app>
     <v-container>
-      <v-card width="600" class="mx-auto mt-12">
+      <v-card v-if="currenciesLoaded" width="600" class="mx-auto mt-12">
         <v-card-title><h1>Converter</h1></v-card-title>
         <v-card-text>
           <v-form>
             <v-row>
               <v-col cols="5">
-                <v-select :items="items" label="Standard"></v-select>
+                <v-autocomplete
+                  :items="currencies"
+                  item-value="code"
+                  item-text="description"
+                  v-model="selectFrom"
+                  :hint="`${selectFrom.code} - ${selectFrom.description}`"
+                  label="Convert from..."
+                  no-data-text="No currency available"
+                  persistent-hint
+                  return-object
+                  single-line
+                ></v-autocomplete>
               </v-col>
               <v-spacer></v-spacer>
               <v-col cols="5">
-                <v-select :items="items" label="Standard"></v-select>
+                <v-autocomplete
+                  :items="currencies"
+                  item-value="code"
+                  item-text="description"
+                  v-model="selectTo"
+                  :hint="`${selectTo.code} - ${selectTo.description}`"
+                  label="Convert to..."
+                  no-data-text="No currency available"
+                  persistent-hint
+                  return-object
+                  single-line
+                ></v-autocomplete>
               </v-col>
             </v-row>
             <v-row>
@@ -20,7 +42,7 @@
               </v-col>
               <v-col cols="2">
                 <v-card-actions>
-                  <v-btn @click="convertCurrencies" class="info" icon>
+                  <v-btn class="info" icon>
                     <v-icon>mdi-repeat</v-icon>
                   </v-btn>
                 </v-card-actions>
@@ -37,14 +59,24 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'App',
   data() {
     return {
-      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      currenciesLoaded: false,
       fromCurrency: 1,
-      toCurrency: 5.48
+      toCurrency: 5.48,
+      selectFrom: { code: 'USD', description: 'United States Dollar' },
+      selectTo: { code: 'BRL', description: 'Brazilian Real' }
     }
+  },
+  created() {
+    this.$store.dispatch('getCurrencies')
+    this.currenciesLoaded = true
+  },
+  computed: {
+    ...mapState(['currencies'])
   }
 }
 </script>
