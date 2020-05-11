056959b203e4939108758f467eed063943b575f3
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-05-11 01:07:14 -0300

    Create store and fetch action/mutation

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,31 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import ConverterService from '../services/ConverterService'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    currencies: null
+  },
+  mutations: {
+    FETCH_CURRENCIES(state, currencies) {
+      state.currencies = currencies
+    }
+  },
+  actions: {
+    getCurrencies({ commit }) {
+      let currenciesObj = []
+      ConverterService.getCurrencies().then(response => {
+        const currenciesKeys = Object.keys(response.data)
+        for (const key of currenciesKeys) {
+          currenciesObj.push({
+            code: key,
+            description: response.data[key]
+          })
+        }
+        commit('FETCH_CURRENCIES', currenciesObj)
+      })
+    }
+  }
+})
